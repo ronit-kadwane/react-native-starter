d15e5a1681d67921ce2826ccec226e72dbbfb591
Ronit Kadwane <ronit@logicwind.com>
2019-11-22 15:57:07 +0530

    eslint added

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -2,12 +2,12 @@
  * @format
  */
 
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
 import 'react-native';
 import React from 'react';
 import App from '../App';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   renderer.create(<App />);
